722aae2fa8788a66feca3300a9c2f62aa8b9c4a6
Miquel Quesada <miquel.quesada@gmail.com>
2023-01-30 22:53:38 +0100

    Set control from vector array, plotting joint positions and sensor velocity

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #sudo apt install ffmpeg
 #git clone https://github.com/deepmind/mujoco
 #env MUJOCO_GL=egl
+#./../../Mujoco/bin/simulate ./anybotics_anymal_c/scene.xml
 
 #@title Other imports and helper functions
 import mujoco
@@ -27,7 +28,7 @@ mujoco.mj_forward(model, data)
 
 print(f"Timestep: {model.opt.timestep}")
 
-DURATION  = 3   # seconds
+DURATION  = 4   # seconds
 FRAMERATE = 60  # Hz
 
 # Make a new camera, move it to a closer distance.
@@ -36,16 +37,21 @@ mujoco.mjv_defaultFreeCamera(model, camera)
 camera.distance = 3
 
 timevals = []
+q_position = []
 q_velocity = []
-x_position = []
+sensordata = []
 frames = []
+
+'''mujoco.mj_step(model, data)
+q_position.append(data.qpos.copy())
+print(q_position)'''
 
 # Simulate and save data
 mujoco.mj_resetDataKeyframe(model, data, 0)
 
 while data.time < DURATION:
-  # Set control vector.
-  movmtrx = np.identity(12)
+  # Set control vector joint aproach
+  '''movmtrx = np.identity(12)
   rbsop = movmtrx [0,:]
   rbsrot = movmtrx [1,:]
   rbkrot = movmtrx [2,:]
@@ -58,14 +64,24 @@ while data.time < DURATION:
   lfsop = movmtrx [9,:]
   lfsrot = movmtrx [10,:]
   lfkrot = movmtrx [11,:]
-  data.ctrl = (lfkrot+rfkrot+lfsrot+rfsrot)*np.sin(data.time)*(-0.2)+(lbkrot+rbkrot+lbsrot+rbsrot)*np.cos(data.time*2)*(-0.2)
+  if data.time < 1: data.ctrl = (lfkrot+rfkrot+lfsrot+rfsrot)*np.sin(data.time)*(-0.2)+(lbkrot+rbkrot+lbsrot+rbsrot)*np.cos(data.time*2)*(-0.2)
+  else: data.ctrl = 0'''
+  
+  # Set control vector array aproach
+  ctrl=np.zeros(12)
+  for i in range(4):
+    ctrl[(i*3)]=1
+    ctrl[(i*3)+2]=1
+  if data.time < 1: data.ctrl = ctrl*(0.2)
+  else: data.ctrl = 0
   
   # Step the simulation.
   mujoco.mj_step(model, data)
   
   timevals.append(data.time)
-  q_velocity.append(data.qvel.copy())
-  x_position.append(data.geom_xpos[2,2])
+  q_position.append(data.qpos[7:].copy())
+  q_velocity.append(data.qvel[8:].copy())
+  sensordata.append(data.sensor('velocimeter').data.copy())
 
   # Render and save frames.
   if len(frames) < data.time * FRAMERATE:
@@ -84,11 +100,14 @@ height = 800
 figsize = (width / dpi, height / dpi)
 _, ax = plt.subplots(2, 1, figsize=figsize, dpi=dpi, sharex=True)
 
-ax[0].plot(timevals, q_velocity)
-ax[0].set_title('q velocity')
-ax[0].set_ylabel('velocity')
+for i in range(20):
+  print(q_position[i])
 
-ax[1].plot(timevals, x_position)
+ax[0].plot(timevals, q_position)
+ax[0].set_title('q position')
+ax[0].set_ylabel('position')
+
+ax[1].plot(timevals, sensordata)
 ax[1].set_xlabel('time (seconds)')
-ax[1].set_ylabel('position')
-_ = ax[1].set_title('x position')
+ax[1].set_ylabel('sensor velocity')
+_ = ax[1].set_title('sensor velocity')
